24a3ff7a00a97ac4697710998fc4fa760bdb75b8
HowardPongYuenLAM <121382449+HowardPongYuenLAM@users.noreply.github.com>
2023-08-29 18:37:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import streamlit as st
 import os
 
 
-def main():
-    st.set_page_config(page_title="Ask your PDF 2023.08.29")
-    st.header("Ask your PDF 💬")
-    response=MY_HUGGINGFACEHUB_API_TOKEN = "abc"
-    st.write(response)
+st.set_page_config(page_title="Ask your PDF 2023.08.29")
+st.header("Ask your PDF 💬")
+response="abc"
+st.write(response)
    
-
-if __name__ == '__main__':
-    main()
